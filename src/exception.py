6fd479fdde519db793140acab721449d0dd73605
ron watson <swapnil71443@gmail.com>
2024-12-09 11:52:53 +0530

    rectified exception

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -25,3 +25,4 @@ if __name__=="__main__":
     except Exception as e:
         logging.info("Divide by 0")
         raise CustomException(e,sys)
+    
